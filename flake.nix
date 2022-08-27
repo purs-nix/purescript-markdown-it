880aba9a692eb6738130aa4681ba5bb8d0cab5e7
Mason Mackaman <masondeanm@aol.com>
2022-08-26 22:45:57 -0400

    Update purs-nix

--- flake.nix
+++ flake.nix
@@ -1,6 +1,5 @@
 { inputs =
-    { easy-ps.follows = "purs-nix/easy-ps";
-      make-shell.url = "github:ursi/nix-make-shell/1";
+    { make-shell.url = "github:ursi/nix-make-shell/1";
       nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
 
       npmlock2nix =
@@ -8,6 +7,7 @@
           url = "github:nix-community/npmlock2nix";
         };
 
+      ps-tools.follows = "purs-nix/ps-tools";
       purs-nix.url = "github:purs-nix/purs-nix/ps-0.14";
       utils.url = "github:ursi/flake-utils/8";
     };
@@ -15,10 +15,9 @@
   outputs = { utils, ... }@inputs:
     with builtins;
     utils.apply-systems { inherit inputs; }
-      ({ make-shell, pkgs, purs-nix, ... }:
+      ({ make-shell, pkgs, ps-tools, purs-nix, ... }:
          let
            l = p.lib; p = pkgs;
-           easy-ps = import inputs.easy-ps { inherit pkgs; };
            npmlock2nix = import inputs.npmlock2nix { inherit pkgs; };
            our-node = p.nodejs-14_x;
            inherit (import ./purs.nix { inherit npmlock2nix our-node p; } purs-nix)
@@ -32,15 +31,14 @@
              make-shell
                { packages =
                    with p;
-                   [ easy-ps.pulp-15_0_0
-
-                     # for npm test
-                     easy-ps.purs-0_13_8
-
-                     esbuild
+                   [ esbuild
                      nodePackages.bower
                      nodePackages.purescript-language-server
                      our-node
+                     ps-tools.pulp-15
+
+                     # for npm test
+                     ps-tools.purescript-0_13_8
 
                      (ps.command
                         { bundle =
